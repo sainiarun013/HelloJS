0e42d21a1b592e7299813586a7c8a7ebc8ceb61b
asaini <saini.arun013@gmail.com>
2019-02-19 14:25:49 +0100

    first commit

---
+++ basic.js
@@ -0,0 +1,71 @@
+console.log("Hello Arun");
+var a = 456;
+var b = 'Arun';
+console.log(a,b);
+
+var d = 45;
+
+if (d==45){
+    console.log('Hi there');
+}
+else if (d ==464){
+    console.log("Go to hell");
+}
+else
+{
+    console.log('You are in heaven');
+}
+
+for (var i=1;i<25;i=i+2)
+{
+    console.log(i);
+}
+
+var j = 1
+while (j<5){
+    console.log(j);
+    j++;
+}
+
+k = 10;
+do {
+    console.log(k);
+}while(k<6)
+
+function add(a,b)
+{
+    return a+b;
+}
+
+console.log(add(2,56));
+var p = new Array();
+r = ["arun",123,"Shailly",546];
+console.log(' lenghth of the array ='+r.length);
+console.log(p.length);
+for (var i=0; i<r.length;i++)
+{
+    console.log(r[i]);
+     }
+console.log('Printing in reverse')
+  for (var i=r.length; i>=0;i--)
+{
+    console.log(r[i]);
+   }
+
+
+   var t = '  arun2saini'
+   console.log(t.charAt(2));
+   var newna = t.concat('   saini   ');
+   console.log(newna);
+   console.log(newna.indexOf('i'));
+   console.log(newna.slice(2,6));
+   console.log(newna.toUpperCase());
+   console.log(t.trim());
+
+   console.log(t.charAt(6));
+
+   var name2 = new String("Arun Saini");
+   console.log(name2);
+   var array1 = new Array();
+
+
